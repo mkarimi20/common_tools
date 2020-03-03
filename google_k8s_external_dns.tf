1337ebc8dc6d4bca0663617e483410b50a172c8d
Farkhod <sadykovfarkhod@gmail.com>
2020-03-03 01:01:46 -0600

    Created terraform mofule for external dns
    Fixed terrafom module and deployed to low lavel clusters

--- google_k8s_external_dns.tf
+++ google_k8s_external_dns.tf
@@ -1,39 +1,18 @@
 
 # Reading the  template and  setting terraform values
-data "template_file" "external_dns_values" {
-  template = "${file("helm-external-dns/external-dns/template_values.yaml")}"
+module "external-dns" {
+  source  = "fuchicorp/chart/helm"
+  version = "0.0.2"
+  deployment_name         = "external-dns-deployment"
+  deployment_environment  = "${var.deployment_environment}"
+  deployment_endpoint     = "${var.google_domain_name}"
+  deployment_path         = "external-dns"
 
-  vars {
-    deployment_endpoint = "${var.google_domain_name}"
-    google_project   = "${var.google_project_id}"
+  template_custom_vars    = {
+    google_project        = "${var.google_project_id}"
   }
 }
 
-# Generating the values.yaml file to do deplopyment
-resource "local_file" "external_dns_helm_chart_values" {
-  content  = "${trimspace(data.template_file.external_dns_values.rendered)}"
-  filename = "helm-external-dns/.cache/external_dns_values.yaml"
-}
-
-
-## Deploy external_dns controller
-resource "helm_release" "external_dns_controller" {
-  depends_on = [
-    "kubernetes_namespace.service_tools",
-    "kubernetes_service_account.tiller",
-    "kubernetes_secret.tiller",
-    "kubernetes_cluster_role_binding.tiller_cluster_rule"
-  ]
-  values = [
-    "${data.template_file.external_dns_values.rendered}"
-  ]
-  name      = "external-dns"
-  chart     = "./helm-external-dns/external-dns"
-  namespace = "${var.deployment_environment}"
-}
-
-
-# External DNS needs secret serviceAccountSecret: "common-service-account"
 resource "kubernetes_secret" "external_dns_secret" {
   metadata {
     name      = "google-service-account"

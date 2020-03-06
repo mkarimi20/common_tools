c1d2a7573a4333187934afd59f51072ed2137ddc
Huseyin Akten <husakten@gmail.com>
2020-03-06 16:53:34 -0600

    module updated

--- google_k8s_service_ingress.tf
+++ google_k8s_service_ingress.tf
@@ -1,20 +1,20 @@
 ## Before deploy ingress for each services it will make sure all services available
-resource "helm_release" "services_ingress" {
-  depends_on = [
-    "kubernetes_deployment.vault_deployment",
-    "kubernetes_service.vault_service",
-    "kubernetes_service.nexus_service",
-    "kubernetes_namespace.service_tools",
-    "kubernetes_service_account.tiller",
-    "kubernetes_secret.tiller",
-    "kubernetes_service.vault_service",
-    "kubernetes_cluster_role_binding.tiller_cluster_rule"
-  ]
+# resource "helm_release" "services_ingress" {
+#   depends_on = [
+#     "kubernetes_deployment.vault_deployment",
+#     "kubernetes_service.vault_service",
+#     "kubernetes_service.nexus_service",
+#     "kubernetes_namespace.service_tools",
+#     "kubernetes_service_account.tiller",
+#     "kubernetes_secret.tiller",
+#     "kubernetes_service.vault_service",
+#     "kubernetes_cluster_role_binding.tiller_cluster_rule"
+#   ]
 
-  wait      = true
-  name      = "services-ingress-${var.deployment_environment}"
-  namespace = "${var.deployment_environment}"
-  chart     = "./main-helm"
-}
+#   wait      = true
+#   name      = "services-ingress-${var.deployment_environment}"
+#   namespace = "${var.deployment_environment}"
+#   chart     = "./main-helm"
+# }
 
 

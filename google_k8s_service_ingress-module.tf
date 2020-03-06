14190059115b6b7ae829864316103ce700ece843
Huseyin Akten <husakten@gmail.com>
2020-03-06 16:45:25 -0600

    yes

--- google_k8s_service_ingress-module.tf
+++ google_k8s_service_ingress-module.tf
@@ -6,45 +6,46 @@ module "ingress_deploy" {
   deployment_endpoint    = "ingress.${var.google_domain_name}"
   deployment_path        = "main-helm"
 
-  template_custom_vars = {
+  # template_custom_vars = {
     
-    nexusport = "${var.nexus_service_port}"
-    vaultport = "${var.vault_service_port}"
-    repo_port = "${var.repo_port}"
-    email     = "${var.email}"
-    domain_name = "${var.google_domain_name}"
+  #   nexusport = "${var.nexus_service_port}"
+  #   vaultport = "${var.vault_service_port}"
+  #   repo_port = "${var.repo_port}"
+  #   email     = "${var.email}"
+  #   domain_name = "${var.google_domain_name}"
+  # }
+
+
+  wait      = true
+  name      = "services-ingress-${var.deployment_environment}"
+  namespace = "${var.deployment_environment}"
+  chart     = "./main-helm"
+
+  set {
+    name  = "nexusport"
+    value = "${var.nexus_service_port}"
+  }
+
+  set {
+    name  = "vaultport"
+    value = "${var.vault_service_port}"
+  }
+
+  set {
+    name  = "repo_port"
+    value = "${var.repo_port}"
+  }
+
+  set {
+    name  = "email"
+    value = "${var.email}"
+  }
+
+  set {
+    name = "domain_name"
+    value = "${var.google_domain_name}"
   }
 }
-
-#   wait      = true
-#   name      = "services-ingress-${var.deployment_environment}"
-#   namespace = "${var.deployment_environment}"
-#   chart     = "./main-helm"
-
-#   set {
-#     name  = "nexusport"
-#     value = "${var.nexus_service_port}"
-#   }
-
-#   set {
-#     name  = "vaultport"
-#     value = "${var.vault_service_port}"
-#   }
-
-#   set {
-#     name  = "repo_port"
-#     value = "${var.repo_port}"
-#   }
-
-#   set {
-#     name  = "email"
-#     value = "${var.email}"
-#   }
-
-#   set {
-#     name = "domain_name"
-#     value = "${var.google_domain_name}"
-#   }
-# }
+}
 
 

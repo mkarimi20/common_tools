0f6451465eba5f137fbce16a720b9bfd20b1a0e1
Huseyin Akten <husakten@gmail.com>
2020-03-06 16:59:00 -0600

    ingress in name removed

--- google_k8s_service_ingress-module.tf
+++ google_k8s_service_ingress-module.tf
@@ -3,7 +3,7 @@ module "ingress_deploy" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "ingress"
   deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "ingress.${var.google_domain_name}"
+  deployment_endpoint    = "${var.google_domain_name}"
   deployment_path        = "main-helm"
 
   template_custom_vars = {

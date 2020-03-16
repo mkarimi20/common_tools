0d7774a4746855f52485eff5fb03a98f6045b528
bkarakas <bkarakasu@gmail.com>
2020-03-16 23:16:07 +0000

    ingress version removed

--- variables.tf
+++ variables.tf
@@ -2,46 +2,36 @@ variable "deployment_environment" {
   default = "tools"
   description = "Namespace of the deployment <It will be created>"
 }
-
 variable "vault_token" {
   description = "Please enter token for Vault."
 }
-
 variable "vault_service_port" {
   default     = 8082
   description = "Please do not change this ports."
 }
-
 variable "nexus_service_port" {
   default     = 8083
   description = "Please do not change this ports."
 }
-
 variable "tiller_version" {
   default     = "v2.11.0"
   description = "Please provide version of the tiller."
 }
-
 variable "tiller_namespace" {
   default     = "kube-system"
   description = "Tiller by default will deploy to kube-system"
 }
-
 variable "repo_port" {
   default = 8085
 }
-
 variable "email" {
   default = "fuchicorpsolutions@gmail.com"
 }
-
 variable "google_project_id" {
   default = "angular-unison-267720"
 }
-
 variable "jenkins" {
   type = "map"
-
   default = {
     admin_user             = "admin"
     admin_password         = "password"
@@ -50,10 +40,8 @@ variable "jenkins" {
     git_token              = "awdiahwd12ehhaiodd"
   }
 }
-
 variable "grafana" {
   type = "map"
-
   default = {
     grafana-version  = "6.0.1"
     grafana_username = "admin"
@@ -61,12 +49,10 @@ variable "grafana" {
     grafana-name     = "grafana"
   }
 }
-
 variable "google_domain_name" {
   default = "fuchicorp.com"
   description = "Please change to your domain name"
 }
-
 variable "deployment_name" {
   default = "common_tools"
 }

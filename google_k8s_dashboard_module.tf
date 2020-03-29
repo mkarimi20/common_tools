d53ff22aafdae04d43ea4c477b0461aab3bbe7c7
mo <mkarimidevops@gmail.com>
2020-03-29 01:11:47 -0500

    dashbord removed

--- google_k8s_dashboard_module.tf
+++
@@ -1,9 +0,0 @@
-module "dashboard_deployer" {
-  source  = "fuchicorp/chart/helm"
-  deployment_name        = "dashboard-${var.deployment_environment}"
-  deployment_environment = "kube-system"
-  deployment_endpoint    = "dashboard.${var.google_domain_name}"
-  deployment_path        = "kubernetes-dashboard"
-
-
-}

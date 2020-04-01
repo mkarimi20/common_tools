e18efef6e7ab0f8c61d667cf443865b8e3a4d1f3
mo <mkarimidevops@gmail.com>
2020-04-01 00:24:10 -0500

    provider chaned

--- kubernetes_provider.tf
+++ kubernetes_provider.tf
@@ -1,16 +1,4 @@
-data "google_client_config" "default" {}
-
-data "google_container_cluster" "mycluster" {
-  name = "fuchicorp_cluster"
-  location = central1
-}
-
 provider "kubernetes" {
-  load_config_file = false
-  host = google_container_cluster.mycluster.endpoint
-  token = data.google_client_config.default.access_token
-
-  client_certificate = base64decode(google_container_cluster.mycluster.master_auth[0].client_certificate)
-  client_key = base64decode(google_container_cluster.mycluster.master_auth[0].client_key)
-  cluster_ca_certificate = base64decode(google_container_cluster.mycluster.master_auth[0].cluster_ca_certificate)
+  config_path = "./config"
+  config_context_auth_info = "fuchicorp_cluster"
 }
